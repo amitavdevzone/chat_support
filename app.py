dbfa0e4803aec97f8c745f55e90357243e4cbaef
Amitav Roy <reachme@amitavroy.com>
2024-10-02 18:47:23 +0530

    05 refactored code, used env for keys and moved methods to file

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 import uvicorn
-import requests
+from utils import send_chat_to_chatwoot
 
 app = FastAPI()
 
 @app.post("/api/webhook")
-async def webhook(request: Request, token):
+async def webhook(request: Request, token) -> Response:
     body = await request.json()
     message = body.get("content")
+
     if message == 'Hi':
-        url =""
-        data = {"content": "Hello. How are you?"}
-        headers = {"api_access_token": ""}
-        response = requests.post(url=url, json=data, headers=headers)
+        account_id = body.get("account")['id']
+        conversation_id = body.get("conversation")['id']
+        data = {"content": f"Hello. Conversation: {conversation_id}"}
+        
+        response = send_chat_to_chatwoot(account_id=account_id, conversation_id=conversation_id, data=data)
         
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=3002, reload=True)
